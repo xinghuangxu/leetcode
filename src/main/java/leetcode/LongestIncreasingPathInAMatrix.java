9d397283ebb2dc5a1d7dea5698b6b8c858f0985a
Leon Xu <xinghuangxu@gmail.com>
2016-03-04 18:56:43 -0600

    LongestIncreasingPathInAMatrixTest

---
+++ src/main/java/leetcode/LongestIncreasingPathInAMatrix.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * Created by k586j642 on 3/4/2016.
+ */
+public class LongestIncreasingPathInAMatrix {
+
+    public int longestIncreasingPath(int[][] matrix) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/leetcode/LongestIncreasingPathInAMatrixTest.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import junit.framework.TestCase;
+import org.testng.annotations.Test;
+
+/**
+ * Created by k586j642 on 3/4/2016.
+ */
+public class LongestIncreasingPathInAMatrixTest extends TestCase {
+    @Test
+    public void test1() {
+        LongestIncreasingPathInAMatrix longestIncreasingPathInAMatrix = new LongestIncreasingPathInAMatrix();
+        int[][] nums = {
+                {9,9,4},
+                {6,6,8},
+                {2,1,1}
+        };
+        int expected = 4;
+        int actual = longestIncreasingPathInAMatrix.longestIncreasingPath(nums);
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void test2() {
+        LongestIncreasingPathInAMatrix longestIncreasingPathInAMatrix = new LongestIncreasingPathInAMatrix();
+        int[][] nums = {
+                {3,4,5},
+                {3,2,6},
+                {2,2,1}
+        };
+        int expected = 4;
+        int actual = longestIncreasingPathInAMatrix.longestIncreasingPath(nums);
+        assertEquals(actual,expected);
+    }
+}
